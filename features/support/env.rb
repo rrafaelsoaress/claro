fb7cfb32880af2c8473620094cc815927f7270dc
Rafael Soares <rafael.soares@fs.com.br>
2018-10-24 13:55:18 -0300

    atualizaçao do env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,22 +3,16 @@ require 'capybara/cucumber'
 require 'rspec'
 require 'site_prism'
 
-# Capybara.configure do |config|
-#  config.default_driver = :selenium
-#  config.default_max_wait_time = 10
-#  config.app_host = 'https://claro-vendas.whitelabel.com.br'
-# end
 
 World(Capybara::DSL)
 World(Capybara::RSpecMatchers)
 
 
-
 Capybara.configure do |config|
     config.default_driver = :selenium
+    default_max_wait_time = 5
 end
 
-Capybara.default_max_wait_time = 5
 
 BROWSER = ENV['BROWSER']
 ENV_TYPE = ENV['ENV_TYPE']
